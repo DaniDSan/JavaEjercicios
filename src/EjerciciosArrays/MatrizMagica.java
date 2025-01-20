aeb9a49df8ee907064f4365c951b9a2a3f5a0724
Dani <danields034@gmail.com>
2025-01-20 02:46:55 +0100

    final

--- src/EjerciciosArrays/MatrizMagica.java
+++ src/EjerciciosArrays/MatrizMagica.java
@@ -4,23 +4,17 @@ public class MatrizMagica {
 
 	public static void main(String[] args) {
 		int[][] array1 = { { 1, 2, 3, 4 }, { 2, 3, 4, 1 }, { 4, 3, 2, 1 }, { 3, 1, 4, 2 } };
-		int[][] array2 = { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
 		/*
 		 * for (int i = 0; i < array1.length; i++) { for (int j = 0; j <
 		 * array1[i].length; j++) { array1[i][j] = i + 1; } }
 		 */
-		System.out.print(Comprobar(array2));
+		System.out.print(Comprobar(array1));
 	}
 
 	public static boolean Comprobar(int[][] array1) {
 		int totalPrimeraFila = 0;
-		int totalPrimeraColumna = 0;
 		for (int j = 0; j < array1[0].length; j++) {
 			totalPrimeraFila += array1[0][j];
-		}
-
-		for (int i = 0; i < array1.length; i++) {
-			totalPrimeraColumna += array1[i][0];
 		}
 
 		for (int i = 1; i < array1.length; i++) {
@@ -32,17 +26,6 @@ public class MatrizMagica {
 				return false;
 			}
 		}
-
-		for (int i = 0; i < array1[i].length; i++) {
-			int totalColumna = 0;
-			for (int j = 1; j < array1.length; j++) {
-				totalColumna += array1[i][j];
-				if (totalColumna != totalPrimeraColumna) {
-					return false;
-				}
-			}
-		}
-
 		return true;
 	}
 }
